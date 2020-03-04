797907cf630eae6e43ae164866da26368f00f010
kck540 <kyle.klenk@usask.ca>
2020-03-04 15:03:57 -0600

    kck540 testing commit

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,7 @@ public class Main {//extends Application{
     public void start(Stage primaryStage) {
 
     }*/
-
+    // Added comment above main
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         PlannerSQL test = new PlannerSQL();
         test.startUp();
