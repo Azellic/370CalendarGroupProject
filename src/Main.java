30f81392eee107b67fe295d5efef0175ceb264c6
Lloyd <llc323@usask.ca>
2020-03-04 17:20:25 -0600

    llc323: committing the base calendar for our project

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ import Model.CalendarItem;
 import Model.CoursesModel;
 import Model.TaskBoardModel;
 import View.*;
+import Calendar.FullCalendarView;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.YearMonth;
 
 // JavaFX packages we need
 import javafx.application.Application;
@@ -16,11 +18,14 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.control.TabPane;
 
 // Main execution for the app
 public class Main extends Application{
@@ -41,16 +46,23 @@ public class Main extends Application{
     GradeSidebar gradeView;
     TaskSidebar taskView;
 
+    // Window set up values
     Rectangle2D bounds;
 
+    // Bounding boxes for the window objects
     BorderPane border;
+    VBox calendarBoundingBox; // To control the size of the calendar
     VBox calendarBox;
     VBox sideBar;
-    HBox buttonBox;
 
-    Button grades;
-    Button tasks;
-    Button day;
+    // Parts of the tab pane
+    TabPane tabPane;
+    Tab grades;
+    Tab tasks;
+    Tab day;
+
+    //
+    FullCalendarView calendarv;
 
     @Override
     public void start(Stage primaryStage)  throws Exception{
@@ -60,60 +72,82 @@ public class Main extends Application{
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
         */
+        /*
+        Code used for display the calendar
+            primaryStage.setTitle("Full Calendar Example");
+        primaryStage.setScene(new Scene(new FullCalendarView(YearMonth.now()).getView()));
+        primaryStage.show();
+         */
+
+        calendarv = new FullCalendarView(YearMonth.now());
+        tabPane = new TabPane();
         border = new BorderPane();
 
-        int w = 400;
+
+        int w = 600;
         int h = 800;
 
         calendarView = new CalendarView(w, h);
 
-        createButtons();
+        createTabs();
+
+        // Set the title
         primaryStage.setTitle("CMPT370 Project");
 
         Screen screen = Screen.getPrimary();
         bounds = screen.getVisualBounds();
 
+        // Set the window size based on the screen bounds
         primaryStage.setX(bounds.getMinX());
         primaryStage.setY(bounds.getMinY());
         primaryStage.setWidth(bounds.getWidth());
         primaryStage.setHeight(bounds.getHeight());
 
-        calendarBox = new VBox(calendarView);
+        // Set the bounds of the calendar
+        calendarBoundingBox = new VBox(calendarv.getView());
+        calendarBoundingBox.setMaxSize((bounds.getWidth()*2/3) - bounds.getWidth()*0.05, bounds.getHeight());
+        calendarBoundingBox.setAlignment(Pos.CENTER);
+        calendarBoundingBox.setStyle("-fx-background-color: lightgrey");
+
+        // set the calender view to the window
+        calendarBox = new VBox(calendarBoundingBox);
         calendarBox.setPrefSize(bounds.getWidth()*2/3, bounds.getHeight());
         calendarBox.setAlignment(Pos.CENTER);
-        
-        buttonBox = new HBox(grades, tasks, day);
-        buttonBox.setPrefSize(bounds.getWidth()/3, 40);
-        buttonBox.setAlignment(Pos.TOP_CENTER);
+        calendarBox.setStyle("-fx-background-color: dimgrey");
 
-        sideBar = new VBox(buttonBox);
+        // Set the buttons to the side bar
+        //buttonBox = new HBox(tabPane);
+        //buttonBox.setPrefSize(bounds.getWidth()/3, 40);
+        //buttonBox.setAlignment(Pos.TOP_CENTER);
+
+        // Set the the buttons on the side bar
+        sideBar = new VBox(tabPane);
         sideBar.setPrefSize(bounds.getWidth()/3, bounds.getHeight());
         sideBar.setAlignment(Pos.TOP_CENTER);
+        sideBar.setStyle("-fx-background-color: darkgrey");
 
+        // Set the two regions onto the main window
         border.setLeft(calendarBox);
         border.setRight(sideBar);
 
+        // Set items in the border into the scene and display the scene
         Scene scene = new Scene(border);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     /*
-        Create the buttons for the window
+        Create the tabs for the sidebar
      */
-    public void createButtons() {
-        grades = new Button("Grades");
-        grades.setPrefHeight(40);
-        grades.setPrefWidth(75);
+    public void createTabs() {
 
+        grades = new Tab("Grades", new Label("Show all the grades available"));
+        tasks = new Tab("Tasks"  , new Label("Show all tasks for the month"));
+        day = new Tab("Day" , new Label("Show all day events, grades, courses"));
 
-        tasks = new Button("Tasks");
-        tasks.setPrefHeight(40);
-        tasks.setPrefWidth(75);
-
-        day = new Button("Day");
-        day.setPrefHeight(40);
-        day.setPrefWidth(75);
+        tabPane.getTabs().add(grades);
+        tabPane.getTabs().add(tasks);
+        tabPane.getTabs().add(day);
 
     }
 
