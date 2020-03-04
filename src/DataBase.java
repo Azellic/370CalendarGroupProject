93938f771ab9654b251bfe3e22dcb66524aeeaa9
kck540 <kyle.klenk@usask.ca>
2020-03-04 17:46:42 -0600

    kck540- added DataBase to git

---
+++ src/DataBase.java
@@ -0,0 +1,160 @@
+import java.sql.*;
+
+public class DataBase {
+    private static Connection con;
+
+    /**
+     * Starts the database
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public void startUp() throws SQLException, ClassNotFoundException {
+        getConnection();
+    }
+    /**
+     * Sets the connection to the database and then calls initialise method
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    private void getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        con = DriverManager.getConnection("jdbc:sqlite:plannerDB.db");
+        initialize();
+    }
+
+    private void initialize() throws SQLException {
+        Statement state = con.createStatement();
+        try (ResultSet courseTable = state.executeQuery("SELECT name FROM sqlite_master " +
+                "WHERE type='table' AND name='course';")) {
+
+            if (!courseTable.next()) {
+                System.out.println("Building the tag table and putting some data in it");
+                state.execute("CREATE TABLE course" +
+                        "(courseID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "courseName VARCHAR);");
+            }
+        }
+
+        ResultSet eventTable = state.executeQuery("SELECT name FROM sqlite_master WHERE " +
+                "type='table' AND name='event';");
+        if (!eventTable.next()){
+            System.out.println("Building the event table and putting some data in it");
+            state.execute("CREATE TABLE event" +
+                    "(eventID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "courseID INTEGER," +
+                    "startTime VARCHAR," +
+                    "endTime VARCHAR," +
+                    "day INTEGER," +
+                    "month INTEGER," +
+                    "year INTEGER," +
+                    "eventTitle VARCHAR," +
+                    "FOREIGN KEY(courseID) REFERENCES course(courseID)" +
+                    "ON DELETE CASCADE);");
+        }
+
+        ResultSet assessmentTable = state.executeQuery("SELECT name FROM sqlite_master WHERE " +
+                "type='table' AND name='assessment';");
+        if(!assessmentTable.next()) {
+            System.out.println("Building the assessment table and putting data in it");
+            state.execute("CREATE TABLE assessment" +
+                    "(assessmentID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "courseID INTEGER," +
+                    "weight INTEGER," +
+                    "grade REAL," +
+                    "assessmentTitle VARCHAR," +
+                    "FOREIGN KEY(courseID) REFERENCES course(courseID)" +
+                    "ON DELETE CASCADE);");
+        }
+
+        ResultSet taskTable = state.executeQuery("SELECT name FROM sqlite_master WHERE " +
+                "type='table' AND name='task';");
+        if(!taskTable.next()) {
+            System.out.println("Building the task table and putting data in it");
+            state.execute("CREATE TABLE task" +
+                    "(taskID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "courseID INTEGER," +
+                    "dueDay INTEGER," +
+                    "dueMonth INTEGER," +
+                    "dueYear INTEGER," +
+                    "taskTitle VARCHAR," +
+                    "completed VARCHAR," +
+                    "FOREIGN KEY(courseID) REFERENCES course(courseID)" +
+                    "ON DELETE CASCADE);");
+        }
+    }
+
+    public void insertCourse(String courseName) throws SQLException {
+        PreparedStatement prep = con.prepareStatement("INSERT INTO course(courseName) VALUES(?);");
+        prep.setString(1,courseName);
+        prep.executeUpdate();
+    }
+
+    public void insertEvent(int courseID, String startTime, String endTime,
+                            int day, int month, int year, String eventTitle) throws SQLException {
+        PreparedStatement prep = con.prepareStatement("INSERT INTO event(courseID, startTime, endTime, day" +
+                ", month, year, eventTitle) VALUES (?,?,?,?,?,?,?);");
+        prep.setInt(1,courseID);
+        prep.setString(2,startTime);
+        prep.setString(3,endTime);
+        prep.setInt(4, day);
+        prep.setInt(5, month);
+        prep.setInt(6,year);
+        prep.setString(7,eventTitle);
+        prep.executeUpdate();
+    }
+
+    public void insertTask(int courseID, int dueDay, int dueMonth, int dueYear, String taskTitle) throws SQLException {
+        PreparedStatement prep = con.prepareStatement("INSERT INTO task(courseID, dueDay, dueMonth, dueYear," +
+                " taskTitle,completed) VALUES(?,?,?,?,?,?);");
+        prep.setInt(1,courseID);
+        prep.setInt(2,dueDay);
+        prep.setInt(3,dueMonth);
+        prep.setInt(4,dueYear);
+        prep.setString(5,taskTitle);
+        prep.setInt(6,0);
+        prep.executeUpdate();
+    }
+
+    public void insertAssessment(int courseID, int weight, double grade, String assessmentTitle) throws SQLException {
+        PreparedStatement prep = con.prepareStatement("INSERT INTO assessment(courseID, WEIGHT, " +
+                "GRADE, ASSESSMENTTITLE) VALUES(?,?,?,?);");
+        prep.setInt(1, courseID);
+        prep.setInt(2, weight);
+        prep.setDouble(3, grade);
+        prep.setString(4, assessmentTitle);
+        prep.executeUpdate();
+    }
+
+    public ResultSet displayEvents() throws SQLException, ClassNotFoundException {
+        if (con == null) {
+            getConnection();
+        }
+        Statement state = con.createStatement();
+        return state.executeQuery("SELECT * FROM event;");
+    }
+
+    public ResultSet displayCourses() throws SQLException, ClassNotFoundException {
+        if (con == null) {
+            getConnection();
+        }
+        Statement state = con.createStatement();
+        return state.executeQuery("SELECT * FROM course");
+    }
+    public ResultSet displayAssessments() throws SQLException, ClassNotFoundException {
+        if (con == null) {
+            getConnection();
+        }
+        Statement state = con.createStatement();
+        return state.executeQuery("SELECT * FROM assessment");
+    }
+    public ResultSet displayTasks() throws SQLException, ClassNotFoundException {
+        if (con == null){
+            getConnection();
+        }
+        Statement state = con.createStatement();
+        return state.executeQuery("SELECT * FROM task");
+    }
+
+}
