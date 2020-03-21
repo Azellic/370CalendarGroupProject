d2c44b6dbea2e6a802b87009270be48dfe114aab
Alexa Armitage (ama043) <alexa.armitage@usask.ca>
2020-03-20 20:37:25 -0600

    [ama043] A bunch of refactoring and add tasks

---
+++ src/View/NewEventDialog.java
@@ -0,0 +1,135 @@
+package View;
+
+import Model.Event;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+import java.awt.*;
+import java.lang.reflect.Field;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+public class NewEventDialog extends InputDialog {
+
+    private final ButtonType doneButtonType;
+    VBox addEventBox;
+    TextField title;
+    TextField desc;
+    ComboBox<String> courseChoice, colourChoice;
+    ToggleGroup startAMPM, endAMPM;
+    DatePicker datePicker;
+    Spinner<Integer> startHour, startMinute, endHour, endMinute;
+    TextField location;
+
+    public NewEventDialog() {
+        super();
+        this.setTitle("Create an Event");
+
+        doneButtonType = new ButtonType("Done", ButtonBar.ButtonData.OK_DONE);
+        this.getDialogPane().getButtonTypes().addAll(doneButtonType, ButtonType.CANCEL);
+
+        addEventBox = new VBox();
+        addEventBox.setPrefWidth(400);
+
+        Label mainLabel = new Label("Enter new event information");
+        mainLabel.setFont(new Font("Arial", 16));
+        title = new TextField();
+        title.setPromptText("Title");
+        Tooltip.install(title, new Tooltip("Hitting done with an empty title is the same as hitting cancel"));
+        desc = new TextField();
+        desc.setPromptText("Description");
+
+        ObservableList<String> courses = FXCollections.observableArrayList("None");
+        //TODO: Generate the list of courses using course model
+        courseChoice = new ComboBox<>(courses);
+        courseChoice.setValue("None");
+
+        ObservableList<String> colours = FXCollections.observableArrayList("Green", "Blue", "Red",
+                "Orange", "Yellow");
+        colourChoice = new ComboBox<>(colours);
+        colourChoice.setValue("Green");
+
+        datePicker = new DatePicker(LocalDate.now());
+        datePicker.setEditable(false);
+
+        startAMPM = new ToggleGroup();
+        endAMPM = new ToggleGroup();
+        startHour = new Spinner<>(1, 12, 1, 1);
+        startMinute = new Spinner<>(0, 59, 0);
+        endHour = new Spinner<>(1, 12, 2, 1);
+        endMinute = new Spinner<>(0, 59, 0, 1);
+        HBox startTimeBox = createTimeBox(startHour, startMinute, startAMPM);
+        HBox endTimeBox = createTimeBox(endHour, endMinute, endAMPM);
+        Tooltip.install(endHour, new Tooltip("End time must be after start time. Will otherwise default to being " +
+                "equal to start time."));
+        Tooltip.install(endMinute, new Tooltip("End time must be after start time. Will otherwise default to " +
+                "being equal to start time."));
+        location = new TextField();
+        location.setPromptText("Location");
+
+        addEventBox.getChildren().addAll(mainLabel, new Label("(Must enter all starred fields)"),
+                new Label("Title (*):"), title, new Label("Description:"), desc,
+                new Label("Course:"), courseChoice, new Label("Colour:"), colourChoice,
+                new Label("Date: (*)"), datePicker, new Label("Start time:"), startTimeBox,
+                new Label("End time:"), endTimeBox, new Label("Location:"), location);
+
+        this.getDialogPane().setContent(addEventBox);
+
+        Platform.runLater(() -> title.requestFocus());
+
+        // Convert the result to an event when the done button is clicked.
+        this.setResultConverter(dialogButton -> createEventOnDoneClicked(dialogButton));
+    }
+
+    private Event createEventOnDoneClicked(Object dialogButton) {
+        if (dialogButton == doneButtonType) {
+            Color c;
+            try {
+                Field field = Class.forName("java.awt.Color").getField(colourChoice.getValue());
+                c = (Color)field.get(null);
+            } catch (Exception e) {
+                c = Color.GREEN; // Not defined
+            }
+
+            if(title.getText().isEmpty()){
+                return null;
+            }
+
+            int mod = 0;
+            if(((RadioButton)startAMPM.getSelectedToggle()).getText().equals("PM")){
+                mod = 12;
+            }
+            LocalTime sTime = LocalTime.of(startHour.getValue()+mod, startMinute.getValue());
+
+            mod = 0;
+            if (((RadioButton)endAMPM.getSelectedToggle()).getText().equals("PM")){
+                mod = 12;
+            }
+            LocalTime eTime = LocalTime.of(endHour.getValue()+mod, endMinute.getValue());
+
+            if(sTime.isAfter(eTime)){
+                eTime = sTime;
+            }
+
+            Event newEvent =  new Event(title.getText(), desc.getText(), null, c,
+                    datePicker.getValue().getDayOfMonth(), datePicker.getValue().getMonthValue(),
+                    datePicker.getValue().getYear(), Time.valueOf(sTime), Time.valueOf(eTime), location.getText());
+
+            if (!courseChoice.getValue().equals("None")){
+                newEvent.setCourse(courseChoice.getValue());
+            }
+
+            return newEvent;
+        }
+        return null;
+    }
+}
